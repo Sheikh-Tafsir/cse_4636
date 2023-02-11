61142e8d551a5fe450c3f4011f4c2e457963b4b7
Sheikh-Tafsir <190041130tafsir@gmail.com>
2023-02-11 23:20:57 +0600

    assignment 2

---
+++ lab_assignment2/src/main/java/com/example/lab_assignment2/HelloServlet.java
@@ -0,0 +1,87 @@
+package com.example.lab_assignment2;
+
+import java.io.*;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        // Hello
+        /*out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");*/
+
+        Cookie [] cookieArray = request.getCookies();
+        Cookie tempCookie = null;
+        int fl=0;
+
+        for (Cookie c:cookieArray)
+        {
+
+            if (c.getName().equals("usernameKey")) {
+                fl = 1;
+                tempCookie = c;
+            }
+        }
+        if(tempCookie==null){
+            RequestDispatcher rd = request.getRequestDispatcher("login.html");
+            rd.include(request, response);
+        }
+        else{
+            out.println("<h1> Successfully logged in </h1>");
+
+            RequestDispatcher rd = request.getRequestDispatcher("Homepage");
+            rd.include(request, response);
+        }
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        // Hello
+        /*out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");*/
+
+        Cookie [] cookieArray = request.getCookies();
+        Cookie tempCookie = null;
+        int fl=0;
+
+        for (Cookie c:cookieArray)
+        {
+
+            if (c.getName().equals("usernameKey")) {
+                fl = 1;
+                tempCookie = c;
+            }
+        }
+        if(tempCookie==null){
+            RequestDispatcher rd = request.getRequestDispatcher("login.html");
+            rd.include(request, response);
+        }
+        else{
+            out.println("<h1> Successfully logged in </h1>");
+
+            RequestDispatcher rd = request.getRequestDispatcher("Homepage");
+            rd.forward(request, response);
+        }
+    }
+
+    public void destroy() {
+    }
+}
